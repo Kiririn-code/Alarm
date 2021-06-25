ae87f9da2cd98cacf36be2c3154c9e0819f2278e
Kiririn <ya.aleks19982@yandex.ru>
2021-06-25 09:54:56 +0300

    fix
    fix

--- Assets/Scripts/PlayAlarm.cs
+++ Assets/Scripts/PlayAlarm.cs
@@ -8,35 +8,39 @@ public class PlayAlarm : MonoBehaviour
     [SerializeField] private float _volumeTime;
 
     private const float _updateTime = 0.1f;
-    private float _volumeRunningTime;
+    private float _volumeRunningTime = 0;
 
     private Coroutine _increaseVolumeSound;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       _audioSourse.Play();
-       _increaseVolumeSound = StartCoroutine(PlaySong(1));
+        _audioSourse.Play();
+        RunCoroutine(1);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-      _increaseVolumeSound = StartCoroutine(PlaySong(0));
+        RunCoroutine(0);
+    }
+
+    private void RunCoroutine(int coriutineState)
+    {
+        if (_increaseVolumeSound != null)
+        {
+            StopCoroutine(_increaseVolumeSound);
+            _volumeRunningTime = 0;
+        }
+        _increaseVolumeSound = StartCoroutine(PlaySong(coriutineState));
     }
 
     private IEnumerator PlaySong(int value)
     {
         var time = new WaitForSeconds(_updateTime);
 
-        if (_increaseVolumeSound != null)
-        {
-            StopCoroutine(_increaseVolumeSound);
-            _volumeRunningTime = 0;
-        }
-
-        while(_audioSourse.volume != value) 
+        while (_audioSourse.volume != value) 
         { 
             _volumeRunningTime += Time.deltaTime;
-            _audioSourse.volume = Mathf.Lerp(_audioSourse.volume, value,_volumeRunningTime/_volumeTime);
+            _audioSourse.volume =Mathf.MoveTowards(_audioSourse.volume, value,_volumeRunningTime/_volumeTime);
             yield return time;
         }
     }
